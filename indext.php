41f0d02fb0f6bd7f08015a80748f225b9869f5e4
Atlantis UDS <mentaricollection90@gmail.com>
2021-02-03 14:59:04 +0700

    menambahkan list

--- indext.php
+++ indext.php
@@ -11,5 +11,10 @@
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<ul>
+		<li>Kemarin</li>
+		<li>Saya Pergi</li>
+		<li>Ke Surabaya</li>
+	</ul>
 </body>
 </html>
